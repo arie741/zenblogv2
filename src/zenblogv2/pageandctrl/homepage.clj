5cfad8bcb05e6e6eff59b1c74cccd250fc997dc9
Sabda PS <squest@users.noreply.github.com>
2015-01-21 20:55:22 +0700

    code style

--- src/zenblogv2/pageandctrl/homepage.clj
+++ src/zenblogv2/pageandctrl/homepage.clj
@@ -5,10 +5,21 @@
             [zenblogv2.dbase :as db]
             [hiccup.core :as hc]))
 
-(defn blog [kjudul kisi kid]
-  (hc/html [:div {:class "panel"} "\n    " [:h2 kjudul] "\n    " [:p kisi "... " [:a {:href (str "/blog/" kid)} "Baca Selengkapnya"]]]))
+(defn blog
+  "It would be nice if you tell others what this function does"
+  [kjudul kisi kid]
+  (hc/html [:div {:class "panel"}
+            "\n    " [:h2 kjudul] "\n    "
+            [:p kisi "... "
+             [:a {:href (str "/blog/" kid)} "Baca Selengkapnya"]]]))
 
 (html/deftemplate home "selmer/home.html"
   []
-  [:blogtemplate] (html/html-content (apply str (map #(blog (:key %) (take 500 (:value %)) (:id %)) (db/get-by-title)))))
+  [:blogtemplate] 
+  ;; This kind of thing is the one suitable using clojure's threading macro of ->> or ->
+  (html/html-content (apply str 
+                            (map #(blog (:key %) (take 500 (:value %)) (:id %)) 
+                                 (db/get-by-title)))))
+                               
+                
 
