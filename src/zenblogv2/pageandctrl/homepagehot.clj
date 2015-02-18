85ba59b4bfa581135de94b18c846361e508443e0
arie741 <arie.pranasakti@zeniuseducation.com>
2015-02-18 19:40:54 +0700

    repairing homepagehot.clj

--- src/zenblogv2/pageandctrl/homepagehot.clj
+++ src/zenblogv2/pageandctrl/homepagehot.clj
@@ -2,8 +2,10 @@
   (:require
    [net.cgrand.enlive-html :as html]
    [zenblogv2.pageandctrl.pagelayout :refer :all]
-   [com.ashafa.clutch :as cl] [zenblogv2.dbase :as db]
-   [hiccup.core :as hc] [zenblogv2.pageandctrl.homepage :as hp]))
+   [com.ashafa.clutch :as cl] 
+   [zenblogv2.dbase :as db]
+   [hiccup.core :as hc] 
+   [zenblogv2.pageandctrl.homepage :as hp]))
 
 (defn blog
   "What is this function do?"
@@ -22,7 +24,6 @@
   ;; And this is an example of using thread-last macro ->>
   (->> (db/sort-by-rating)
        (map #(hp/blog (:title %) (take 500 (:isi %)) (:_id %)))
-       (map #(blog (:title %) (take 500 (:isi %)) (:id %)))
        (apply str)
        (html/html-content)))
 
